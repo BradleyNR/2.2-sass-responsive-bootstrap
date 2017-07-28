2a673a42d9d0e3a22be972d378069e1edc80def2
Bradley_Carson <bmcarson1671@gmail.com>
2017-07-28 14:34:57 -0400

    corrected scaling on small screens

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
   'use strict';
 
 
-var left = document.getElementById('bottomsection').clientHeight;
-var right = document.getElementById('bigform').clientHeight;
+  var left = document.getElementById('bottomsection').clientHeight;
+  var right = document.getElementById('bigform').clientHeight;
 
 
 function matchSizes(){
+   left = document.getElementById('bottomsection').clientHeight;
+   right = document.getElementById('bigform').clientHeight;
+
   if(left>right){
     document.getElementById('bigform').style.height=left + 'px';
   }else{
     document.getElementById('bottomsection').style.height=right + 'px';
   }
-
-  setTimeout(matchSizes, 500);
+ console.log('hello');
+  // setTimeout(matchSizes, 500);
 }
 
 
@@ -21,6 +24,11 @@ console.log(left);
 console.log(right);
 
 
+
+
 matchSizes()
+
+window.addEventListener("resize", matchSizes);
+
 
     }());
