9d566639cd6ebed4209d02d565efa09a461809fd
Bradley_Carson <bmcarson1671@gmail.com>
2017-07-28 16:10:20 -0400

    reenabled and fixed JS

--- main.js
+++ main.js
@@ -14,14 +14,13 @@ function matchSizes(){
    left = document.getElementById('bottomsection').clientHeight;
    right = document.getElementById('bigform').clientHeight;
 
-  if(left>right){
+  if(left > right){
     document.getElementById('bigform').style.height=left + 'px';
-  }else{
-    document.getElementById('bottomsection').style.height=right + 'px';
   }
  console.log('hello');
   // setTimeout(matchSizes, 500);
 }
+
 
 
 console.log(left);
